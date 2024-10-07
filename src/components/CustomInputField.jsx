d04fbea8cd98b2ac4cc42dfb26ec4f9a6dce3f41
miejejaw <mierafjejaw@gmail.com>
2024-10-07 16:10:47 +0300

    remove focus outline when an error exist

--- src/components/CustomInputField.jsx
+++ src/components/CustomInputField.jsx
@@ -2,17 +2,18 @@ import PropTypes from "prop-types";
 
 const CustomInputField = ({label, type, placeholder, register, errors}) => {
     const fieldId = label.toLowerCase().replace(' ', '_').trim();
+
     return (
         <div className="mb-4">
-            <label htmlFor={label.toLowerCase().replace(' ', '-').trim()} className="block text-gray-700 mb-2">
+            <label htmlFor={fieldId} className="block text-gray-700 mb-2">
                 {label}:
             </label>
             <input
                 type={type}
                 id={fieldId}
-                className={`w-full px-3 py-2 border ${errors ? 'border-red-500' : 'border-light-purple'} rounded-3xl focus:outline-none focus:ring-1 focus:ring-light-purple`}
+                className={`w-full px-3 py-2 border rounded-3xl ${errors ? 'border-red-500 focus:outline-none' : 'border-light-purple focus:outline-none focus:ring-light-purple focus:ring-1'}`}
                 placeholder={placeholder}
-                {...register(fieldId, {required: true})}
+                {...register}
             />
             {errors && <p className="text-red-500 text-sm mt-1">{errors.message}</p>}
         </div>
@@ -23,7 +24,7 @@ CustomInputField.propTypes = {
     label: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     placeholder: PropTypes.string.isRequired,
-    register: PropTypes.func.isRequired,
+    register: PropTypes.object.isRequired,  // Expect an object from react-hook-form
     errors: PropTypes.object
 };
 
